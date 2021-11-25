17f4885f1861fa91f59be63f5056fe91c76e853b
kange873 <63007885+kange873@users.noreply.github.com>
2021-11-24 21:00:48 -0800

    Add files via upload

---
+++ public/signup.js
@@ -0,0 +1,7 @@
+
+document.getElementById('submit').addEventListener('click', function (req, res) {
+    console.log("info submitted");
+    console.log(res)
+    alert("You have been registered!");
+});
+
